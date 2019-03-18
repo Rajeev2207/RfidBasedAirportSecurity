225fa3a03ed3fa4854c1e7d1ee624dbad37d34a2
Rajeev Suri <rajeevsuri040989@gmail.com>
2019-03-18 00:38:12 -0700

    New Repo, Finally Working Project

---
+++ RfidBasedAirportSecurity/Controllers/LuggagesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RfidBasedAirportSecurity.Models;
+
+namespace RfidBasedAirportSecurity.Controllers
+{
+    public class LuggagesController : ApiController
+    {
+        private RfidBasedAirportSecurityContext db = new RfidBasedAirportSecurityContext();
+
+        // GET: api/Luggages
+        public IQueryable<Luggage> GetLuggages()
+        {
+            return db.Luggages;
+        }
+
+        // GET: api/Luggages/5
+        [ResponseType(typeof(Luggage))]
+        public async Task<IHttpActionResult> GetLuggage(string id)
+        {
+            Luggage luggage = await db.Luggages.FindAsync(id);
+            if (luggage == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(luggage);
+        }
+
+        // PUT: api/Luggages/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutLuggage(string id, Luggage luggage)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != luggage.Luggage_RFID_Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(luggage).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LuggageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Luggages
+        [ResponseType(typeof(Luggage))]
+        public async Task<IHttpActionResult> PostLuggage(Luggage luggage)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Luggages.Add(luggage);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (LuggageExists(luggage.Luggage_RFID_Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = luggage.Luggage_RFID_Id }, luggage);
+        }
+
+        // DELETE: api/Luggages/5
+        [ResponseType(typeof(Luggage))]
+        public async Task<IHttpActionResult> DeleteLuggage(string id)
+        {
+            Luggage luggage = await db.Luggages.FindAsync(id);
+            if (luggage == null)
+            {
+                return NotFound();
+            }
+
+            db.Luggages.Remove(luggage);
+            await db.SaveChangesAsync();
+
+            return Ok(luggage);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool LuggageExists(string id)
+        {
+            return db.Luggages.Count(e => e.Luggage_RFID_Id == id) > 0;
+        }
+    }
+}
---
+++ RfidBasedAirportSecurity/Models/PassengerAreaAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RfidBasedAirportSecurity.Models
+{
+    public class PassengerAreaAccess
+    {
+        [Key]
+        public string ZoneId { get; set; }  // Primary Key
+
+        [ForeignKey("RfidInventory")]
+        public string RFID_ID { get; set; }
+        public RfidInventory RfidInventory { get; set; }
+
+        //[DatabaseGenerated(DatabaseGeneratedOption.Computed)]  // similar to SYSDATE
+        public DateTime Access_Time { get; set; }
+
+    }
+}
